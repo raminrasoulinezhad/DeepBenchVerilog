85456103659c8e3e0eb6fd7d1cf03fc093b83508
raminrasoulinezhad <ramin_r348@yahoo.com>
2021-03-17 15:23:56 -0600

    update

--- codes/CNNs/keras_generator/exps.sh
+++ codes/CNNs/keras_generator/exps.sh
@@ -7,14 +7,17 @@
 # in_y / 2
 python3 keras_conv_generator.py --batch=32 --in_x=161 --in_y=350 --in_c=1   --filters=32  --kernel_x=5 --kernel_y=20 --strides=2 --padding=valid --dilation_rate=1 --groups=1 
 
+# Done
 #python3 keras_conv_generator.py --batch=8  --in_x=54  --in_y=54  --in_c=64  --filters=64  --kernel_x=3 --kernel_y=3  --strides=1 --padding=same  --dilation_rate=1 --groups=1 
-# in_x / 2 , in_y / 2
-python3 keras_conv_generator.py --batch=8  --in_x=28  --in_y=28  --in_c=64  --filters=64  --kernel_x=3 --kernel_y=3  --strides=1 --padding=same  --dilation_rate=1 --groups=1 
+# in_x / 2 , in_y / 2 , c_in / 4 , filters / 4
+python3 keras_conv_generator.py --batch=8  --in_x=28  --in_y=28  --in_c=16  --filters=16  --kernel_x=3 --kernel_y=3  --strides=1 --padding=same  --dilation_rate=1 --groups=1 
 
+# Done
 # python3 keras_conv_generator.py --batch=16 --in_x=224 --in_y=224 --in_c=3   --filters=64  --kernel_x=3 --kernel_y=3  --strides=1 --padding=same  --dilation_rate=1 --groups=1
 # in_x / 4 , in_y / 4 , filters / 4
 python3 keras_conv_generator.py --batch=16 --in_x=56 --in_y=56 --in_c=3   --filters=16  --kernel_x=3 --kernel_y=3  --strides=1 --padding=same  --dilation_rate=1 --groups=1
 
+# Done
 #python3 keras_conv_generator.py --batch=16 --in_x=7   --in_y=7   --in_c=512 --filters=512 --kernel_x=3 --kernel_y=3  --strides=1 --padding=same  --dilation_rate=1 --groups=1
 #filters / 32 , in_c / 32
 python3 keras_conv_generator.py --batch=16 --in_x=7   --in_y=7   --in_c=16 --filters=16 --kernel_x=3 --kernel_y=3  --strides=1 --padding=same  --dilation_rate=1 --groups=1
