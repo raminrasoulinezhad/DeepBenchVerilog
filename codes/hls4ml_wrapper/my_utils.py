c592f497f11add0bf253a1f9fd478804b38a6c67
raminrasoulinezhad <ramin_r348@yahoo.com>
2021-03-16 23:55:33 -0600

    update

--- codes/hls4ml_wrapper/my_utils.py
+++ codes/hls4ml_wrapper/my_utils.py
@@ -1,5 +1,5 @@
 from urllib.request import urlretrieve
-from hls4ml.utils.config import create_vivado_config
+from hls4ml.utils.config import create_vivado_config,_load_example_config
 import pprint
 import json
 import yaml
